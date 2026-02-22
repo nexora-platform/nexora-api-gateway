541d96eba1c7a599dfb8677e945ad329b81731f3
Ali Al-Jalo <ali.aljalo@gmail.com>
2026-02-22 18:39:15 +0300

    feat: enable discovery client registration in api gateway

--- src/main/java/com/nexora_api_getway/NexoraApiGetwayApplication.java
+++ src/main/java/com/nexora_api_getway/NexoraApiGetwayApplication.java
@@ -2,8 +2,10 @@ package com.nexora_api_getway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NexoraApiGetwayApplication {
 
 	public static void main(String[] args) {
