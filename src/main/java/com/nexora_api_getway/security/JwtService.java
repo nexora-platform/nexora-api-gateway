2549c89294b9d1a3879eed585c33e203d6fbf74b
Ali Al-Jalo <ali.aljalo@gmail.com>
2026-03-02 15:30:07 +0300

    gateway: implement JWT parsing service

--- src/main/java/com/nexora_api_getway/security/JwtService.java
+++ src/main/java/com/nexora_api_getway/security/JwtService.java
@@ -3,20 +3,30 @@ package com.nexora_api_getway.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
 
 @Service
 public class JwtService {
 
     @Value("${jwt.secret}")
-    private String secret;
+    private String jwtSecret;
+
+    private SecretKey secretKey;
+
+    @PostConstruct
+    void init() {
+        this.secretKey = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+    }
 
     public Claims extractClaims(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(Keys.hmacShaKeyFor(secret.getBytes()))
+        return Jwts.parser()
+                .verifyWith(secretKey)
                 .build()
-                .parseClaimsJws(token)
-                .getBody();
+                .parseSignedClaims(token)
+                .getPayload();
     }
 }
